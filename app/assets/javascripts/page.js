260b5b83bc2c41aa04940c86a1bb8891c21f69a1
Gabriela Rotver <gabrielarotver86@gmail.com>
2016-08-05 15:42:43 -0400

    got search bar to work on main map

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -1,7 +1,10 @@
+var handler;
 $(document).on('turbolinks:load', function(){
 
 
   if(window.location.pathname === "/") {
+    $('#geocoding_fields').show();
+    $('#geocoding').addClass('active');
     $.ajax("/events.json").done(function(event){
       var events = event;
       console.log(events[1].latitude);
@@ -11,35 +14,69 @@ $(document).on('turbolinks:load', function(){
 
         // work with googlemaps APIs
         handler = Gmaps.build('Google');
-        handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
 
-        for(var i = 0; i < events.length; i++) {
-          markers = handler.addMarkers([
-            {
-              "lat": events[i].latitude,
-              "lng": events[i].longitude,
-              "infowindow": events[i].event_name + "<br>" + events[i].street_address
-            }
-          ]);
+        var mapOptions = {
+          provider:{
+            zoom: 10,
+            mapTypeId: google.maps.MapTypeId.NORMAL,
+            panControl: true,
+            scaleControl: false,
+            streetViewControl: true,
+            overviewMapControl: true,
+          },
+          internal: {id: 'map-canvas'}
+        };
+
+        handler.buildMap(mapOptions, function(){
+          handler.map.centerOn({lat: organizations[0].latitude, lng: organizations[0].longitude});
+          for(var i = 0; i < events.length; i++) {
+            markers = handler.addMarkers([
+              {
+                "lat": events[i].latitude,
+                "lng": events[i].longitude,
+                "infowindow": "Event: " +events[i].event_name + "<br>" + events[i].street_address
+              }
+            ]);
+          }
+
+          for(var i = 0; i < organizations.length; i++) {
+            markers = handler.addMarkers([
+              {
+                "lat": organizations[i].latitude,
+                "lng": organizations[i].longitude,
+                "infowindow": "Org: " + organizations[i].org_name + "<br>" + organizations[i].street_address
+              }
+            ]);
         }
 
-        for(var i = 0; i < organizations.length; i++) {
-          markers = handler.addMarkers([
-            {
-              "lat": organizations[i].latitude,
-              "lng": organizations[i].longitude,
-              "infowindow": organizations[i].org_name + "<br>" + organizations[i].street_address
-            }
-          ]);
-        }
-
-        handler.bounds.extendWith(markers);
-        handler.fitMapToBounds();
         });
+          $("#submit_button_geocoding").click(function(){
+            var geocoding  = new google.maps.Geocoder();
+            codeAddress(geocoding, handler);
+          });
 
       });
      });
+
   }
-
-
 });
+
+function codeAddress(geocoding){
+  console.log(handler);
+  var address = $("#search_box_geocoding").val();
+  if(address.length > 0){
+    geocoding.geocode({'address': address},function(results, status){
+      if(status == google.maps.GeocoderStatus.OK){
+        handler.map.centerOn(results[0].geometry.location);
+        handler.getMap().setZoom(13);
+        var marker  =  new google.maps.Marker({
+          position: results[0].geometry.location
+        });
+        }else{
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }else{
+    alert("Search field can't be blank");
+  }
+}
