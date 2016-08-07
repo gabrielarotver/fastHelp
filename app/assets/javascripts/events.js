21ad5e6d016b8911c279cea04d2144de84a4ebb4
Gabriela Rotver <gabrielarotver86@gmail.com>
2016-08-07 14:54:59 -0400

    added hover on calendar, some css

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -3,8 +3,30 @@ $(document).ready(function() {
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
-        events: '/events.json'
+      // put your options and callbacks here
+      events: '/events.json',
+
+      // calendar will only have the necessary # of rows per month
+      fixedWeekCount: false,
+
+      eventMouseover: function(calEvent, jsEvent) {
+          var tooltip = '<div class="tooltipevent">' + calEvent.title + '</div>';
+          $("body").append(tooltip);
+          $(this).mouseover(function(e) {
+              $(this).css('z-index', 10000);
+              $('.tooltipevent').fadeIn('500');
+              $('.tooltipevent').fadeTo('10', 1.9);
+          }).mousemove(function(e) {
+              $('.tooltipevent').css('top', e.pageY + 10);
+              $('.tooltipevent').css('left', e.pageX + 20);
+          });
+      },
+
+      eventMouseout: function(calEvent, jsEvent) {
+           $(this).css('z-index', 8);
+           $('.tooltipevent').remove();
+      },
+
     })
 
 });
