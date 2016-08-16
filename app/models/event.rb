045fb962f98d0990524c577205a0156b97af598b
Gabriela Rotver <gabrielarotver86@gmail.com>
2016-08-16 19:02:02 -0400

    fixed event's default extension to correct one

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,7 +12,7 @@ class Event < ApplicationRecord
   validates :state, presence: true
   validates :zip_code, presence: true
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}, :default_url => '/system/events/eventsDefault.jpg'
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}, :default_url => '/system/events/eventsDefault.png'
 
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
